28ee339ac1cabb40d1f0f02bfee4c3ad953eb5a0
Ollie Haydon-Mulligan <ollieh-m@users.noreply.github.com>
2018-05-02 21:05:19 +0100

    Load more posts if we need to go further back to get all posts sent since the given get_posts_since timestamp

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -2,11 +2,13 @@ _crawlHelper = require('../helpers/crawlHelper')
 
 exports.index = async (req, res, next) => {
   try {
-    var crawlHelper = new _crawlHelper
+    var crawlHelper = new _crawlHelper({get_posts_since: req.query.get_posts_since})
 
     await crawlHelper.login()
 
     crawlHelper.crawler.on("complete", () => {
+      console.log("complete")
+
       res.render("posts/index", {
         data: crawlHelper.posts.sort()
       });
--- app/helpers/date.js
+++ app/helpers/date.js
@@ -2,9 +2,9 @@ const moment = require('moment');
 const chrono = require('chrono-node');
 
 exports.convertFacebookDate = (dateStr) => {
-  let isRelative = dateStr.includes("ago") 
-    || dateStr.includes('Yesterday') 
-    || dateStr.includes('hrs') 
+  let isRelative = dateStr.includes("ago")
+    || dateStr.includes('Yesterday')
+    || dateStr.includes('hrs')
     || dateStr.includes('mins');
 
   if (dateStr.includes('hrs')) {
@@ -14,10 +14,24 @@ exports.convertFacebookDate = (dateStr) => {
   }
 
   if (isRelative) {
-    dateStr = chrono.parseDate(dateStr);    
+    dateStr = chrono.parseDate(dateStr);
   }
 
   date = moment(dateStr, 'D MMMM at hh:mm');
 
   return date.valueOf();
 };
+
+exports.convertStandardDate = (string) => {
+  // takes ISO date (including timezone) and converts to date object
+  return moment(string).valueOf()
+}
+
+exports.prettyFormatRawDate = (date) => {
+  // takes date object and converts to date object in local timezone
+  return moment(date).format('MMMM Do YYYY, h:mm:ss a')
+}
+
+exports.isBefore = (dateInQuestion, comparitorDate) => {
+  return moment(dateInQuestion).isBefore(comparitorDate)
+}
