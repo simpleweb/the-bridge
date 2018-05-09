8027b45d34b765f0dc98bd83b149b52f94b0e75f
Ollie Haydon-Mulligan <ollieh-m@users.noreply.github.com>
2018-05-09 14:12:42 +0100

    Get react client working with websocket endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from "react"
+import Websocket from 'react-websocket'
+import Posts from "./components/Posts"
+
 import "./App.css"
 
 class App extends Component{
+  constructor(props) {
+    super(props)
+    this.state = {posts: []}
+  }
+
+  handleData(data) {
+    let result = JSON.parse(data);
+
+    this.setState({
+      posts: [...result.posts, ...this.state.posts],
+      got_posts_since: result.got_posts_since,
+      got_posts_before: result.got_posts_before
+    })
+  }
+
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <Posts posts={this.state.posts}/>
+        <Websocket url='ws://localhost:8080/postsstream' onMessage={this.handleData.bind(this)} reconnect={false}/>
       </div>
     );
   }
+
 }
 
 export default App;
